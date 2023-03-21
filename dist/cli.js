8267b9b32a36c7dc8431da804197f551520c36e1
RetrowPoLo <100202418+RetrowPoLo@users.noreply.github.com>
2023-03-21 13:14:13 +0000

    feat: add init for prettier: config, prettierignore and install

--- dist/cli.js
+++ dist/cli.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 import ncp from 'ncp';
 import { promisify } from 'util';
 import { execa } from 'execa';
+import { writeFile, readFile } from 'fs/promises';
 import { projectInstall } from 'pkg-install';
 import arg from 'arg';
 import fs from 'fs';
@@ -60,14 +61,47 @@ async function initHusky(targetDir) {
 	return true;
 }
 
+const PRETTIER_VERSION = '^2.8.6';
+
 async function initPrettier(targetDir) {
-	const result = await execa('echo', [' {} > .prettierrc.json'], {
-		cwd: targetDir,
-	});
-	if (result.failed) {
+	// Config for the .prettierrc.json file
+	const prettierConfig = {
+		trailingComma: 'es5',
+		useTabs: true,
+		tabWidth: 4,
+		semi: true,
+		singleQuote: true,
+		printWidth: 100,
+		bracketSameLine: true,
+	};
+	// Config for the .prettierignore file
+	const prettierignoreConfig = ['node_modules', 'vscode', 'package-lock.json'].join('\n');
+	try {
+		// Prettier config file creation
+		await writeFile(`${targetDir}/.prettierrc.json`, JSON.stringify(prettierConfig));
+		// Prettier ignore file creation
+		await writeFile(`${targetDir}/.prettierignore`, prettierignoreConfig);
+		// Read the package.json file
+		const packageJsonPath = `${targetDir}/package.json`;
+		const packageJsonString = await readFile(packageJsonPath, 'utf-8');
+		const packageJson = JSON.parse(packageJsonString);
+		// Add prettier as a dev dependency
+		packageJson.devDependencies = {
+			...packageJson.devDependencies,
+			prettier: PRETTIER_VERSION,
+		};
+		// Add a command to the package.json to run prettier
+		packageJson.scripts = {
+			...packageJson.scripts,
+			prettier: 'prettier --write .',
+		};
+		// Write the updated package.json file
+		await writeFile(packageJsonPath, JSON.stringify(packageJson));
+		return true;
+	} catch (err) {
+		console.error(err);
 		return Promise.reject(new Error('Failed to initialize prettier'));
 	}
-	return true;
 }
 
 function installPackages(targetDir) {
--- src/utils/init-prettier.ts
+++ src/utils/init-prettier.ts
@@ -1,13 +1,51 @@
-import { execa } from 'execa';
+import { readFile, writeFile } from 'fs/promises';
+import { PRETTIER_VERSION } from '../versions';
 
 export async function initPrettier(targetDir: string) {
-	const result = await execa('echo', [' {} > .prettierrc.json'], {
-		cwd: targetDir,
-	});
+	// Config for the .prettierrc.json file
+	const prettierConfig = {
+		trailingComma: 'es5',
+		useTabs: true,
+		tabWidth: 4,
+		semi: true,
+		singleQuote: true,
+		printWidth: 100,
+		bracketSameLine: true,
+	};
 
-	if (result.failed) {
+	// Config for the .prettierignore file
+	const prettierignoreConfig = ['node_modules', 'vscode', 'package-lock.json'].join('\n');
+
+	try {
+		// Prettier config file creation
+		await writeFile(`${targetDir}/.prettierrc.json`, JSON.stringify(prettierConfig));
+
+		// Prettier ignore file creation
+		await writeFile(`${targetDir}/.prettierignore`, prettierignoreConfig);
+
+		// Read the package.json file
+		const packageJsonPath = `${targetDir}/package.json`;
+		const packageJsonString = await readFile(packageJsonPath, 'utf-8');
+		const packageJson = JSON.parse(packageJsonString);
+
+		// Add prettier as a dev dependency
+		packageJson.devDependencies = {
+			...packageJson.devDependencies,
+			prettier: PRETTIER_VERSION,
+		};
+
+		// Add a command to the package.json to run prettier
+		packageJson.scripts = {
+			...packageJson.scripts,
+			prettier: 'prettier --write .',
+		};
+
+		// Write the updated package.json file
+		await writeFile(packageJsonPath, JSON.stringify(packageJson));
+
+		return true;
+	} catch (err) {
+		console.error(err);
 		return Promise.reject(new Error('Failed to initialize prettier'));
 	}
-
-	return true;
 }
